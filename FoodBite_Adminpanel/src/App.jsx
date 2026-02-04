6a04fb0d76749d048083321a0fae2755dd5bb37a
impriyanshusingh01 <impriyanshsingh2003@gmail.com>
2026-02-05 00:39:05 +0530

    Initial commit

---
+++ FoodBite_Adminpanel/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddFood from './components/Add food/AddFood'
+
+const App = () => {
+  return (
+    <AddFood />
+  )
+}
+
+export default App
---
+++ src/main/java/com/foodBite/FoodBite/FoodBiteApplication.java
@@ -0,0 +1,13 @@
+package com.foodBite.FoodBite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBiteApplication.class, args);
+	}
+
+}
